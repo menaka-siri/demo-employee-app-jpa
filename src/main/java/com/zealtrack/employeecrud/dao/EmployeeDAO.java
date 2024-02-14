4bae9dad8b72d408f169fabf92588a5e3b0d8607
Menaka S <menaka442@gmail.com>
2024-02-14 11:28:52 +0530

    initial commit

---
+++ src/main/java/com/zealtrack/employeecrud/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package com.zealtrack.employeecrud.dao;
+
+import com.zealtrack.employeecrud.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
