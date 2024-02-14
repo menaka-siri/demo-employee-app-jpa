dabcb08000168de7b6b4e5aa328983b4eb1bc1cf
Menaka S <menaka442@gmail.com>
2024-02-14 11:58:38 +0530

    replace DAO implementation with JpaRepository pattern

--- src/main/java/com/zealtrack/employeecrud/rest/EmployeeController.java
+++ src/main/java/com/zealtrack/employeecrud/rest/EmployeeController.java
@@ -1,6 +1,5 @@
 package com.zealtrack.employeecrud.rest;
 
-import com.zealtrack.employeecrud.dao.EmployeeDAO;
 import com.zealtrack.employeecrud.entity.Employee;
 import com.zealtrack.employeecrud.service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
